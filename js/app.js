9c981d0951e0a3f212a20740441c2cf61547ba79
MisbahMuib <misbahmuib2016@gmail.com>
2021-09-04 16:09:51 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const API_KEY = `db4d9474a6b4082a2dc8182e8a514096`;
+
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temp', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    //set weather icon
+    const url = `https://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imageIcon = document.getElementById('weather-icon');
+    imageIcon.setAttribute('src', url);
+
+
+
+}
+
